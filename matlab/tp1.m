f56692c6763a0fa2d0a46d0307b8fa186536c50f
christian <clbonomi@gmail.com>
2018-04-08 23:30:12 -0300

    anda!!!

--- matlab/tp1.m
+++ matlab/tp1.m
@@ -1,4 +1,7 @@
-function tp1(nombreArchivo, p)
+function tp1()
+format short
+nombreArchivo = "/home/christian/tests_tp1/test_aleatorio.txt"
+p = 0.85
 W = construirMatriz(nombreArchivo)%importdata("/home/christian/matriz"," ",0); %nos guardamos la matriz del archivo en W
 [m,n] = size(W) % n tiene el tamaño de W
 c_j = sum(W) % c_j es un vector con la cantidad de links que referencian a  cada pagina
@@ -7,12 +10,13 @@ c_j = sum(W) % c_j es un vector con la cantidad de links que referencian a  cada
 D = diag(arrayfun(@(x) dividir_si_es_posible(x),c_j)) 
 I=eye(n) % I = matriz identidad
 b = ones(n,1) % b = vector columna de de unos
-sol = (I-p.*D*W)\b % resolvemos el sistema
+sol = (I-p.*W*D)\b % resolvemos el sistema
 b_ordenado = sort(sol)
 escala = sum(b_ordenado) 
 arrayfun(@(x) x/escala,sol)
-
-%I-p.*D*W
+%whos D
+%sol
+%I-p.*W*D
 
 
 function y = dividir_si_es_posible(x)
@@ -22,20 +26,25 @@ else
     y = 1/x
 end
 
+function num = convertir_a_numerico(str)
+   str_tam = cell2mat(str)
+   num = str2double(str_tam)
+
 function ret = construirMatriz(nombreArchivo)
     datos = importdata(nombreArchivo," ",2)
-    posiciones_donde_hay_1 = datos.data
-    str_tam = cell2mat(datos.textdata(1))
-    n = str2double(str_tam)
-    ret = zeros(n,n) %sparse
+    datos(1)
+    if class(datos) == "cell"
+        n = convertir_a_numerico(datos(1))
+        ret = zeros(n,n) %sparse
+    else
+        n = convertir_a_numerico(datos.textdata(1))
+        ret = zeros(n,n) %sparse
+        posiciones_donde_hay_1 = datos.data
 
-    for i = 1:size(posiciones_donde_hay_1,1)
-      ret(posiciones_donde_hay_1(i,:)) = 1
+        for i = 1:size(posiciones_donde_hay_1,1)
+            ret(posiciones_donde_hay_1(i,2), posiciones_donde_hay_1(i,1)) = 1
+        end
+
     end
-    ret;
     
-    
-
-
-
             
